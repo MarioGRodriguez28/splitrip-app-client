eaf01bd2ab1a5a75c60146ea59a43ce972489d9f
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-03-15 13:10:29 +0200

    Gastos con nombre

--- src/components/ListExpenses.jsx
+++ src/components/ListExpenses.jsx
@@ -1,25 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import {
   getAllExpensesService,
   deleteExpensesService,
 } from "../services/expenses.services";
+import {getUsersService} from "../services/auth.services";
 import GastosForm from "./GastosForm";
 
 function ListExpenses() {
   const [allExpenses, setAllExpenses] = useState(null);
   const [isFetching, setIsFetching] = useState(true);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     getData();
+    getUsers();
   }, []);
 
   const getData = async () => {
     setIsFetching(true);
     try {
       const response = await getAllExpensesService();
-      // console.log(response);
       setAllExpenses(response.data);
       setIsFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await getUsersService();
+      setUsers(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -46,6 +57,11 @@ function ListExpenses() {
     }
   };
 
+  const getUsernameById = (id) => {
+    const user = users && users.find((user) => user._id === id);
+    return user ? user.username : "Desconocido";
+  };
+
   const getTotalExpenses = () => {
     if (allExpenses) {
       return allExpenses.reduce((total, expense) => {
@@ -56,7 +72,7 @@ function ListExpenses() {
     }
   };
 
-  if (isFetching === true) {
+  if (isFetching === true || users === null) {
     return <h3>... spinners</h3>;
   }
 
@@ -72,12 +88,11 @@ function ListExpenses() {
         return (
           <div key={eachExpense._id}>
             <p>
-              {eachExpense.id_user} : {eachExpense.item} : {eachExpense.ammount}{" "}
-              &#128176;
+              {getUsernameById(eachExpense.id_user)} : {eachExpense.item} :{" "}
+              {eachExpense.ammount} &#128176;
               <button
                 className="boton1"
-                onClick={() => handleDeleteExpense(eachExpense._id)}
-              >
+                onClick={() => handleDeleteExpense(eachExpense._id)}>
                 <span className="icocor1">&#10060; </span>
               </button>
             </p>
